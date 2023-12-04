539cf9c0a57f93100452a5a335b5b90d312f9071
ShidoB <52756533+ShidoB@users.noreply.github.com>
2023-12-04 16:21:07 +0100

    Update vueStage
    Pas encore fini, rajouter promotion ++ etc

--- lcs-gestionStages/modele/ModeleStage.php
+++ lcs-gestionStages/modele/ModeleStage.php
@@ -3,20 +3,33 @@
 include("Connexion.php");
 include("classes/classeStage.php");
 
-
-
-// Traiter le formulaire
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $etudiantId = $_SESSION['user']->id;
-    $periode = $_POST['periode'];
-    $organisation = $_POST['organisation'];
-    $maitreDeStageMail = $_POST['maitre_de_stage_mail'];
-    $maitreDeStageTelephone = $_POST['maitre_de_stage_telephone'];
-    $taches = $_POST['taches'];
-    $avancementConvention = 'à faire';
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $maitreStage = $_POST['maitreStage'];
+    $profRef = $_POST['profRef'];
+    $mailMaitreStage = $_POST['mailMaitreStage'];
+    $descriptionStage = $_POST['descriptionStage'];
+    $commentMaitreStage = $_POST['commentMaitreStage'];
+    $adr = $_POST['adr'];
+    $cP = $_POST['cP'];
+    $ville = $_POST['ville'];
+    $pays = $_POST['pays'];
+    $conventionElevePDF = $_POST['conventionElevePDF'];
+    $idEtudiant = $_POST['idEtudiant'];
+    $idProf = $_POST['idProf'];
+    $idOrga = $_POST['idOrga'];
+    $idPeriode = $_POST['idPeriode'];
+    $promo = $_POST['promo'];
+    
 
     // Enregistrement du stage dans la base de données
-    $sqlStage = "INSERT INTO stages (idEtudiant, idPeriode, idOrga, maitreStage, telMaitreStage, mailMaitreStage, descriptionStage, taches, avancementConvention) VALUES ('$etudiantId', '$periode', '$organisation', '$maitreDeStageMail', '$maitreDeStageTelephone', '$taches', '$avancementConvention')";
+    $sqlStage = "INSERT INTO stage (idEtudiant, idPeriode, idOrga, maitreStage, telMaitreStage, mailMaitreStage, descriptionStage, taches, avancementConvention) VALUES ('$etudiantId', '$periode', '$organisation', '$maitreDeStageMail', '$maitreDeStageTelephone', '$taches', '$avancementConvention')";
     $conn->query($sqlStage);
 
     header("Location: modeleStage.php");
--- lcs-gestionStages/vue/vueStage.php
+++ lcs-gestionStages/vue/vueStage.php
@@ -8,23 +8,50 @@
 <body>
     <h2>Saisir les informations du stage</h2>
     <form method="post" action="modeleStage.php">
-        <!-- Ajoutez les champs du formulaire ici -->
-        <label for="periode">Période:</label>
-        <input type="text" name="periode" required><br>
 
-        <label for="organisation">ID de l'Organisation:</label>
-        <input type="text" name="organisation" required><br>
+        <label for="promo">Promotion :</label>
+        <select name="promo" required>
+            <option value="">--Veuillez choisir une option--</option>
+            <option value=""></option>
+            <option value=""></option>
+            <option value="">   </option>
+        </select>
 
-        <label for="maitre_de_stage_mail">E-mail du Maître de stage:</label>
-        <input type="text" name="maitre_de_stage_mail" required><br>
 
-        <label for="maitre_de_stage_telephone">Téléphone du Maître de stage:</label>
-        <input type="text" name="maitre_de_stage_telephone" required><br>
+        <label for="maitreStage">Nom du maître de stage:</label>
+        <input type="text" name="maitreStage" required><br>
 
-        <label for="taches">Résumé des tâches:</label>
-        <textarea name="taches" required></textarea><br>
+        <label for="profRef">Nom du Professeur Référent :</label>
+        <input type="text" name="profRef" required><br>
 
-        <button type="submit">Soumettre</button>
+        <label for="mailMaitreStage">Adresse mail du maître du stage :</label>
+        <input type="email" name="mailMaitreStage" required><br>
+
+        <label for="descriptionStage">Description du stage :</label><br>
+        <textarea name="descriptionStage" rows="4" cols="50" required></textarea><br>
+
+        <label for="commentMaitreStage">Commentaire du maître de stage :</label><br>
+        <textarea name="commentMaitreStage" rows="4" cols="50" required></textarea><br>
+
+        <label for="adr">Adresse :</label>
+        <input type="text" name="adr" required><br>
+        
+        <label for="cP">Code Postal :</label>
+        <input type="text" name="cP" required><br>
+        
+        <label for="ville">Ville :</label>
+        <input type="text" name="ville" required><br>
+
+        <label for="pays">Pays :</label>
+        <input type="text" name="pays" required><br>
+
+        <label for="periode">Période:</label><br>
+        <label for="de">De</label>
+        <input type="date" name="date1" required><br>
+        <label for="à">à</label>
+        <input type="date" name="date2" required><br>
+
+        <br><button type="submit">Soumettre</button>
     </form>
 </body>
 </html>
