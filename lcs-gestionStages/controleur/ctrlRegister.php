7a1ecc854a093b3f95a0b3580cc3135dcf948b2d
paolo <p4olo.bruno@gmail.com>
2023-12-11 12:54:03 +0100

    Auto stash before merge of "dev" and "origin/dev"

--- lcs-gestionStages/controleur/ctrlRegister.php
+++ lcs-gestionStages/controleur/ctrlRegister.php
@@ -7,3 +7,31 @@
         header('Location: ./?action=defaut');
     }
     
+    if(isset($_SESSION['login']) && $_SESSION['admin'] == true){
+    include "$racine/vue/vueEntete.php";
+
+    // Nouvelle condition pour l'action "ajouter_utilisateur"
+    if(isset($_GET['action']) && $_GET['action'] == 'ajouter_utilisateur'){
+        // Traitement du formulaire d'ajout d'utilisateur
+        if(isset($_POST['username']) && isset($_POST['password'])){
+            $newUser = [
+                'username' => $_POST['username'],
+                'password' => password_hash($_POST['password'], PASSWORD_DEFAULT)
+                // Vous devriez également valider et sécuriser les données avant de les utiliser
+            ];
+
+            // Ajoutez le nouvel utilisateur à votre système de stockage (base de données, fichier, etc.)
+            // Exemple avec un tableau de session pour stocker temporairement les utilisateurs
+            $_SESSION['users'][] = $newUser;
+
+            echo "Utilisateur ajouté avec succès!";
+        }
+    } else {
+        include "$racine/vue/vueRegister.php";
+    }
+
+    include "$racine/vue/vuePied.php";
+} else {
+    header('Location: ./?action=defaut');
+}
+?>
